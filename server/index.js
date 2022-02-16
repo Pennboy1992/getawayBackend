4f57626ab5795aea41c66cc01ad30408f7af8dff
jpeebles02 <justin1peebles@gmail.com>
2022-02-16 09:13:17 -0800

    moved cors middleware

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 import rootRouter from "./controllers/rootRouter.js";
 
-dotenv.config()
+dotenv.config();
 
 //initialize app
 const app = express();
@@ -13,9 +13,8 @@ const app = express();
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(rootRouter);
-
 app.use(cors());
+app.use(rootRouter);
 
 const port = process.env.PORT || 8080;
 
@@ -26,9 +25,8 @@ mongoose
       console.log(`Server running on port: ${port}`);
 
       app.get("/", (req, res) => {
-        res.send("Welcome to Getaways 2022 API!")
-      })
-
+        res.send("Welcome to Getaways 2022 API!");
+      });
     });
   })
   .catch((err) => {
